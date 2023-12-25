44bc28461531e7bf57dc0aebf00139cba8fa76de
pritom5928 <pritompal.softifybd@gmail.com>
2023-12-26 00:21:21 +0600

    migrate all identity server related configuration data to database by efcore

---
+++ ISCodeMaze/Migrations/MigrationManager.cs
@@ -0,0 +1,66 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using ISCodeMaze.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace ISCodeMaze.Migrations
+{
+    public static class MigrationManager
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                using (var context = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>())
+                {
+                    try
+                    {
+                        context.Database.Migrate();
+                        if (!context.Clients.Any())
+                        {
+                            foreach (var client in InMemoryConfig.GetClients())
+                            {
+                                context.Clients.Add(client.ToEntity());
+                            }
+                            context.SaveChanges();
+                        }
+                        if (!context.IdentityResources.Any())
+                        {
+                            foreach (var resource in InMemoryConfig.GetIdentityResources())
+                            {
+                                context.IdentityResources.Add(resource.ToEntity());
+                            }
+                            context.SaveChanges();
+                        }
+                        if (!context.ApiScopes.Any())
+                        {
+                            foreach (var apiScope in InMemoryConfig.GetApiScopes())
+                            {
+                                context.ApiScopes.Add(apiScope.ToEntity());
+                            }
+                            context.SaveChanges();
+                        }
+                        if (!context.ApiResources.Any())
+                        {
+                            foreach (var resource in InMemoryConfig.GetApiResources())
+                            {
+                                context.ApiResources.Add(resource.ToEntity());
+                            }
+                            context.SaveChanges();
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+
+            return host;
+        }
+    }
+}
--- ISCodeMaze/Program.cs
+++ ISCodeMaze/Program.cs
@@ -1,20 +1,47 @@
 using ISCodeMaze.Configuration;
+using ISCodeMaze.Migrations;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
+//services.AddIdentityServer()
+//        .AddInMemoryApiScopes(InMemoryConfig.GetApiScopes())
+//        .AddInMemoryApiResources(InMemoryConfig.GetApiResources())
+//        .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+//        .AddTestUsers(InMemoryConfig.GetUsers())
+//        .AddInMemoryClients(InMemoryConfig.GetClients())
+//        .AddDeveloperSigningCredential();
+
+
+#region new code for tranfering from In-Memory config to ef core databases
+var migrationAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+
 services.AddIdentityServer()
-        .AddInMemoryApiScopes(InMemoryConfig.GetApiScopes())
-        .AddInMemoryApiResources(InMemoryConfig.GetApiResources())
-        .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
-        .AddTestUsers(InMemoryConfig.GetUsers())
-        .AddInMemoryClients(InMemoryConfig.GetClients())
-        .AddDeveloperSigningCredential();
+    .AddTestUsers(InMemoryConfig.GetUsers())
+    .AddDeveloperSigningCredential() //not something we want to use in a production environment
+    .AddConfigurationStore(opt =>
+    {
+        opt.ConfigureDbContext = c => c.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection"),
+            sql => sql.MigrationsAssembly(migrationAssembly));
+    })
+    .AddOperationalStore(opt =>
+    {
+        opt.ConfigureDbContext = o => o.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection"),
+            sql => sql.MigrationsAssembly(migrationAssembly));
+    });
+
+
+#endregion
 
 services.AddControllersWithViews();
 
 var app = builder.Build();
+
+//app.MigrateDatabase();
 
 app.UseStaticFiles();
 app.UseRouting();
